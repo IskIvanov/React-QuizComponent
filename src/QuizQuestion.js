1fbca75bd0c57ccab2b97eb56c7a2cd3be05c2c2
Iskren <iskren.ivanov@accenture.com>
2019-05-13 15:35:00 +0300

    Complete module2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class QuizQuestion extends Component {
 	render() {
-	  return (
-		<main>
-			<section>
-			<p>// instruction text goes here</p>
-			</section>
+		return (
+			<main>
+				<section>
+				<p>// instruction text goes here</p>
+				</section>
 
-			<section className="buttons">
-			<ul>
-				<li>{this.props.quiz_question.answer_options[0]}</li>
-			</ul>
-			</section>
-		</main>
-		  )
+				<section className="buttons">
+				<ul>
+					<li>{this.props.quiz_question.answer_options[0]}</li>
+				</ul>
+				</section>
+			</main>
+		)
 	}
   }
-
-  export default QuizQuestion
+export default QuizQuestion
