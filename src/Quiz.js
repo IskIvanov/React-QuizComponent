1f2b35a23a9d0183ce8c359667817f6c0207e284
Iskren <iskren.ivanov@accenture.com>
2019-05-13 15:00:07 +0300

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,10 +4,12 @@ let quizData = require('./quiz_data.json');
 
 
 class Quiz extends Component {
+	
 	constructor(props){
 		super(props)
-		this.state = {quiz_pozition: 1}
+		this.state = { quiz_position: 1 }
 	}
+
 	render() {
 		return (
 			<div>
