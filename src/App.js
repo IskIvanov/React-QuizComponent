bb3ca1e044d02331f4130e5db8a1cda465cf495c
Iskren <iskren.ivanov@accenture.com>
2019-05-13 14:49:16 +0300

    Completed the module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import Quiz from './Quiz';
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div />
-    )
+		<div>
+			<h1></h1>
+			<Quiz />
+		</div>
+		)
   }
 }
 
